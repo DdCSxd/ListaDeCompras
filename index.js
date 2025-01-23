9e464acbf20a6b14bf880bd321ef4aecddd36bb1
DdCSxd <125690368+DdCSxd@users.noreply.github.com>
2025-01-23 17:12:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const inputitem = document.getElementById("input-item")
+const listaDeCompras = document.getElementById("lista-de-compras");
+const botaoAdicionar = document.getElementById("adicionar-item");
+let contador = 0;
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    if(inputitem.value === ""){
+        alert("Por favor, insira um item!");
+        return
+    }
+
+    const itemDaLista = document.createElement("li");
+    const containerItemDaLista = document.createElement("div");
+    containerItemDaLista.classList.add("lista-item-container");
+    const inputCheckbox = document.createElement("input");
+    inputCheckbox.type = "checkbox" ;
+    inputCheckbox.id = "checkbox-" + contador++;
+    const nomeItem = document.createElement("p");
+    nomeItem.innerText = inputitem.value;
+
+    inputCheckbox.addEventListener("click", function(){
+        if(inputCheckbox.checked) {
+            nomeItem.style.textDecoration = "line-through";
+        } else{
+            nomeItem.style.textDecoration = "none"
+        }
+    })
+
+    containerItemDaLista.appendChild(inputCheckbox);
+    containerItemDaLista.appendChild(nomeItem);
+
+    itemDaLista.appendChild(containerItemDaLista);
+    
+    const diaDaSemana = new Date().toLocaleDateString("pt-Br", {
+        weekday: "long"
+    });
+    const data = new Date().toLocaleDateString("pt-Br");
+    const hora = new Date().toLocaleTimeString("pt-Br", {
+        hour: "numeric",
+        minute: "numeric"
+    });
+    const dataCompleta = `${diaDaSemana} (${data}) às ${hora}`
+    const itemData = document.createElement("p");
+    itemData.innerText = dataCompleta;
+    itemData.classList.add("texto-data");
+    itemDaLista.appendChild(itemData);
+    
+    
+    listaDeCompras.appendChild(itemDaLista);
+
+    
+    
+})
